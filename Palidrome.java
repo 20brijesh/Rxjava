011552210663899dba37ec3fa50bacfd6eb08d6d
root <root@localhost.localdomain>
2019-12-27 16:33:16 +0530

    dfsdfsdfsdfsdff

---
+++ Palidrome.java
@@ -0,0 +1,26 @@
+class Palidrome{
+
+public static void main(String args[]){
+int rev=0,number;
+int num=121;
+while(num!=0){
+
+rev=rev*10+num%10;
+num=num/10;
+
+
+
+
+
+}
+
+
+
+System.out.println(rev);
+
+
+
+}
+
+
+}
