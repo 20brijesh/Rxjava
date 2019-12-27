3f9f2af835cdd4ca18268b7876c7ead4c75f12b6
root <root@localhost.localdomain>
2019-12-27 16:48:30 +0530

    Sorting

---
+++ Sort.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+class Sort{
+public static void main(String args[]){
+
+
+
+int[] arr={4,5,5,6,5,7,8,5};
+String[] strArr = {"g", "a", "u","o","i"};
+
+Arrays.sort(arr);
+Arrays.sort(strArr);
+System.out.println(Arrays.toString(arr));
+System.out.println(Arrays.toString(strArr));
+}
+
+
+}
